a8f646df95f4a1fcf7b4f7d17dec6eebf442bfeb
Mandy Mozart <mandymozart@viennastruggle.com>
2022-11-06 11:48:57 +0100

    using netlify function as endpoint

--- litit.js
+++ litit.js
@@ -1,15 +1,12 @@
-let PROJECT_ID = "hejdcb8v";
-let DATASET = "production";
-let QUERY = encodeURIComponent('*[_type == "note"]');
-
 // Compose the URL for your project's endpoint and add the query
-let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+let container = document.querySelector("[data-litit]");
+let URL = `https://litit.netlify.app/user-notes?key=${container.dataset.litit}`;
+
 
 // fetch the content
 fetch(URL)
   .then((res) => res.json())
   .then(({ result }) => {
-    let container = document.querySelector("[data-hook='litit']");
     container.innerHTML = JSON.stringify(result);
   })
   .catch((err) => console.error(err));
